01af04d1b8a270fe21700092cc1b1c7b75541ce4
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2024-04-17 01:12:13 +0500

    add product

--- bublik/models.py
+++ bublik/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
