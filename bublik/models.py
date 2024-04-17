df4080a42cbf87cb039bc30af41f2711e6d51bea
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2024-04-17 13:09:15 +0500

    make models and add migrations

--- bublik/models.py
+++ bublik/models.py
@@ -1,9 +1,26 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.name + " - " + str(self.price)
+class Order(models.Model):
+    created_date = models.DateTimeField(default=timezone.now)
+    # cash, card, perevod
+    payment_type = models.CharField(max_length=10)
+    products = models.ManyToManyField(Product, through="Orderposition")
+
+    def __str__(self):
+        return str(self.id)
+    
+class Orderposition(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+    def __str__(self):
+        return str("ord_" + str(self.order.id) + "_" + self.product.name)
