cb89fd1c1a0e87025ede135461866e703ec7b340
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2024-04-20 15:24:41 +0500

    add css for product list

--- bublik/views.py
+++ bublik/views.py
@@ -3,5 +3,9 @@ from .models import Product
 
 # Create your views here.
 def product_list(request):
-    products = Product.objects.filter()
-    return render(request, 'bublik/product_list.html', {'products': products})
+    products = Product.objects.all()
+    
+    # Разбиваем список продуктов на части по 2 элемента
+    products_chunks = [products[i:i + 2] for i in range(0, len(products), 2)]
+    
+    return render(request, 'bublik/product_list.html', {'products_chunks': products_chunks})
