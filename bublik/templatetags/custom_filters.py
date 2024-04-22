4ab3656e8b47dc987386c1bcf9d725e157e47132
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2024-04-22 18:40:40 +0500

    fie empty data bug

--- bublik/templatetags/custom_filters.py
+++ bublik/templatetags/custom_filters.py
@@ -2,6 +2,14 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='to_float')
+def to_float(value):
+    """Преобразует значение в число с плавающей точкой."""
+    try:
+        return float(value)
+    except (ValueError, TypeError):
+        return 0.0
+
 @register.filter(name='multiply')
 def multiply(value, arg):
     """Умножает значение на аргумент."""
